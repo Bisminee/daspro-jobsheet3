c1b6f61c433193a0d5bdc25b2bc23fa1947111c7
Bisminee <fiksilmiah@gmail.com>
2024-09-11 11:19:49 +0700

    tugas 1 Jobsheer 3

--- Kafe07.java
+++ Kafe07.java
@@ -5,7 +5,7 @@ public class Kafe07 {
 
         boolean keanggotaan;
         int jmlKopi, jmlTeh, jmlRoti;
-        double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga, nominalBayar;
+        double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0, totalHarga, totalByte, nominalBayar, nominalInt;
         float diskon = 10 / 100f;
 
         System.out.print("Masukkan keanggotan (true/false): ");
@@ -18,10 +18,12 @@ public class Kafe07 {
         jmlRoti = input.nextInt();
 
         totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
-        nominalBayar = totalHarga - (diskon * totalHarga);
+        totalByte = (byte) totalHarga;
+        nominalBayar = totalByte - (diskon * totalByte);
+        nominalInt = (int) nominalBayar;
 
         System.out.println("Keanggotaan pelanggan " + keanggotaan);
         System.out.println(String.format("Item pembelian %s kopi, %s teh, %s roti", jmlKopi, jmlTeh, jmlRoti));
-        System.out.println("Nominal bayar Rp " + nominalBayar);
+        System.out.println("Nominal bayar Rp " + nominalInt);
     }
 }
---
+++ tugas1jobsheet3_07.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class tugas1jobsheet3_07 {
+    public static void main(String[] args) {
+        Scanner cin = new Scanner(System.in);
+
+        double pListrik, tagihan;
+        int tarif = 1500;
+        
+        System.out.println("Masukkan Penggunaan Listrik Anda dalam kWh: ");
+        pListrik = cin.nextDouble();
+        tagihan = pListrik * tarif;
+        boolean lebih = pListrik > 500;
+        System.out.println("Tagihan listrik anda Rp " + tagihan);
+        System.out.println("apakah melebihi 500 kWh? " + lebih);
+    }    
+}
